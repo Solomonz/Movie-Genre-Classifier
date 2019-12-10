cfbc6660b3a09148db27cafbde72d0f90e0d91fe
Sol Zitter <solomonzitter@gmail.com>
2019-12-10 17:22:57 -0500

    improved scraper, scraped as many synopses as I could, found another dataset where we could maybe get synopses if we wanted

--- scraper.py
+++ scraper.py
@@ -1,40 +1,71 @@
 #! /usr/bin/env python3
 
+from csv import DictReader
 from bs4 import BeautifulSoup
 import requests
-from json import dump
+from json import dump, load
 from tqdm import tqdm
+from random import shuffle
 
 
 def scrape_synopsis(imdb_id):
-    req = requests.get("https://www.imdb.com/title/{}/plotsummary".format(imdb_id))
+    s = requests.Session()
+    s.trust_env = False
+    req = s.get("https://www.imdb.com/title/{}/plotsummary".format(imdb_id))
     soup = BeautifulSoup(req.text, 'html.parser')
     synopsis_h4 = soup.find('h4', string="Synopsis")
     synopsis_li = synopsis_h4.nextSibling.nextSibling.find('li')
     return synopsis_li.getText()
 
 
+def save_to_file(synopses):
+    with open('full_synopses.json', 'r') as synopses_file:
+        existing = load(synopses_file)
+
+    existing.update(synopses)
+
+    with open('full_synopses.json', 'w') as synopses_file:
+        dump(existing, synopses_file, indent=None, separators=(',', ':'))
+
+
 def main():
-    with open('all_simple_data.txt', 'r') as data_file:
-        next(data_file)  # skip past the header row
+    with open('full_synopses.json', 'r') as synopses_file:
+        known_synopses = load(synopses_file)
+
+    #known_synopses = {imdbid: known_synopses[imdbid] for imdbid in filter(lambda i: known_synopses[i].find('It looks like we don\'t have a Synopsis for this title yet. Be the first to contribute! Just click the "Edit page" button') == -1, known_synopses.keys())}
+#
+    #with open('full_synopses.json', 'w') as sf:
+        #dump(known_synopses, sf, indent=None, separators=(',', ':'))
+#
+    #exit(0)
+
+    with open('data/the-movies-dataset/movies_metadata.csv', 'r') as data_file:
+        reader = DictReader(data_file)
         imdb_ids = set()
-        for line in data_file:
-            tokens = line.strip().split('|||')
-            if len(tokens) >= 3 and tokens[2][:2] == 'tt':
-                imdb_ids.add(tokens[2])
+        for row in reader:
+            imdb_ids.add(row['imdb_id'])
                 
-        #imdb_ids = {line.strip().split('|||')[2] for line in data_file}
+    imdb_ids.difference_update(known_synopses)
+    imdb_ids = list(imdb_ids)
+    shuffle(imdb_ids)
 
     imdbID_to_synopsis = {}
 
     for imdb_id in tqdm(imdb_ids):
-        imdbID_to_synopsis[imdb_id] = scrape_synopsis(imdb_id)
+        try:
+            imdbID_to_synopsis[imdb_id] = scrape_synopsis(imdb_id)
+        except Exception as e:
+            import pdb;pdb.set_trace()
+            pass
 
-    with open('full_synopses.json', 'w') as synopses_file:
-        print('dumping synopses')
-        dump(imdbID_to_synopsis, synopses_file, indent=None, separators=(',', ':'))
-        print('finished dumping synopses')
+        if len(imdbID_to_synopsis) >= 200:
+            save_to_file(imdbID_to_synopsis)
+            return
+
+
+    save_to_file(imdbID_to_synopsis)
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
